b4dbe04f9d5e418bcdca247407091b5bbab18ff3
unknown <jaimeyaguem@gmail.com>
2025-05-12 10:17:57 +0200

    OK and ERROR responses done

--- status.py
+++ status.py
@@ -1,7 +1,11 @@
 import requests
 
 def comprobar_estado():
-    r = requests.get("http://api.ejemplo.com/estado")
+    r = requests.get("http://api.ejemplo.com/estado", verify=False)
+    if r.status_code == 200:
+        print("OK!")
+    else:
+        print("ERROR")
 
     print(r)
 
